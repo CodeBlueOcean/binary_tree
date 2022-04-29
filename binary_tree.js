6018ca502984ef7c07ad91e63311acc216c29d66
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-04-28 22:19:54 -0500

    Add files via upload

---
+++ binary_tree.js
@@ -0,0 +1,11 @@
+// Binary Tree Structure - only have 0,1,2 , Perfect Binary Tree a node has either one children or two children and bottom is compeletly fill and Full Binary Tree eithier 0 node or 2 child 
+function BinaryTreeNode(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
+
+// Binary Search Tree            Lookup O(log N)
+// Insert                        Insert O(log N)
+// Delete                        O(log N)
+
